edb9e16e5596aedb7540cd320d93aa8da22eb12c
JO SEUNGHYUN <32855863+KENEW@users.noreply.github.com>
2020-12-04 22:55:12 +0900

    Add files via upload

---
+++ 피보나치_메모이제이션.cpp
@@ -0,0 +1,27 @@
+//#include<iostream>
+//using namespace std;
+//
+//int num[100];
+//
+//int fibonacci(int x)
+//{
+//	if (x <= 2)
+//	{
+//		return 1;
+//	}
+//	if (num[x] != 0)
+//	{
+//		return num[x];
+//	}
+//	else
+//	{
+//		num[x] = fibonacci(x - 1) + fibonacci(x - 2);
+//		return num[x];
+//	}
+//}
+//
+//int main(void)
+//{
+//	printf("%d", fibonacci(30));
+//	return 0;
+//}
