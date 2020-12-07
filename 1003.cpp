9dde5f5d4514e71d365ccb7d5200cac1ce9f8dc5
JO SEUNGHYUN <32855863+KENEW@users.noreply.github.com>
2020-12-07 23:07:04 +0900

    Update 1003.cpp

--- 1003.cpp
+++ 1003.cpp
@@ -1,50 +1,50 @@
-//#include<iostream>
-//using namespace std;
-//
-//long long fiboarr[100] = { 0, 1, };
-//
-//int count0 = 0;
-//int count1 = 0;
-//
-//long long Solution(int N)
-//{
-//	if (N == 0)
-//	{
-//		count0++;
-//		return fiboarr[N];
-//	}
-//	if (N == 1)
-//	{
-//		count1++;
-//		return fiboarr[N];
-//	}
-//	else if (fiboarr[N] == 0)
-//	{
-//		fiboarr[N] = Solution(N - 2) + Solution(N - 1);
-//	}
-//	
-//	return fiboarr[N];
-//}
-//int main(void)
-//{
-//	ios_base::sync_with_stdio(false);
-//	cin.tie(NULL);
-//	cout.tie(NULL);
-//
-//	int N;
-//	cin >> N;
-//
-//	for (int i = 0; i < N; i++)
-//	{
-//		int temp;
-//		cin >> temp;
-//
-//		count0 = 0;
-//		count1 = 0;
-//
-//	
-//			Solution(temp);
-//			cout << count0 << ' ' << count1 << '\n';
-//	}
-//	return 0;
-//}
+#include<iostream>
+using namespace std;
+
+long long fiboarr[100] = { 0, 1, };
+
+int count0 = 0;
+int count1 = 0;
+
+long long Solution(int N)
+{
+	if (N == 0)
+	{
+		count0++;
+		return fiboarr[N];
+	}
+	if (N == 1)
+	{
+		count1++;
+		return fiboarr[N];
+	}
+	else if (fiboarr[N] == 0)
+	{
+		fiboarr[N] = Solution(N - 2) + Solution(N - 1);
+	}
+	
+	return fiboarr[N];
+}
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N;
+	cin >> N;
+
+	for (int i = 0; i < N; i++)
+	{
+		int temp;
+		cin >> temp;
+
+		count0 = 0;
+		count1 = 0;
+
+	
+			Solution(temp);
+			cout << count0 << ' ' << count1 << '\n';
+	}
+	return 0;
+}
